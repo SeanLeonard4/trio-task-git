342fa4d408798c381a5127ff74b7d63fabcad0df
Ubuntu <ubuntu@ip-172-31-17-61.eu-west-1.compute.internal>
2021-03-25 14:03:26 +0000

    Testing webhook

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -11,6 +11,6 @@ sudo apt install -y curl jq
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
 sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
+#testing webhook
 #login
 echo $DOCKERHUB_PASSWORD | docker login --username sleonard9704 --password-stdin
