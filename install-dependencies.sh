8bc97c15e84ca5fb829de3dfa5f1acbee2c33af2
Ubuntu <ubuntu@ip-172-31-17-61.eu-west-1.compute.internal>
2021-03-25 11:57:48 +0000

    docker install update

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-sudo apt-get update
 
-#docker install
+#install docker
+sudo apt-get update
 curl https://get.docker.com | sudo bash
 sudo usermod -aG docker jenkins
 
-#docker compose install
-
-# make sure jq & curl is installed
-sudo apt-get install -y curl jq
-# set which version to download (latest)
+#install docker compose
+sudo apt install -y curl jq
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
-# download to /usr/local/bin/docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-# make the file executable
 sudo chmod +x /usr/local/bin/docker-compose
 
+#login
 echo $DOCKERHUB_PASSWORD | docker login --username sleonard9704 --password-stdn
